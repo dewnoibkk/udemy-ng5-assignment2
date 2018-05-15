c72894671e20dbe41eed4602b009d159b41d482b
Taipan Prasithpongchai <dewnoibkk@gmail.com>
2018-05-15 20:13:56 +0700

    initial repository

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  username: string;
+
+  isButtonEnabled(): boolean {
+    return this.username === null || this.username === '';
+  }
+
+  foo(): void {
+    this.username = '';
+  }
 }
